937b660ba31f67da6d58c745ef02741d65e2ae53
David <tawei.huang1@gmail.com>
2016-07-12 14:10:31 +0800

    Correct the model_storage process

--- data_simulations/data_generator.py
+++ data_simulations/data_generator.py
@@ -6,98 +6,38 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def test1(alpha,T):
+class LinearPayoffSimulation():
     ''' Linear payoff '''
-    h1 = history.MemoryHistoryStorage()
-    m1 = model.MemoryModelStorage()    
-    d = 5
-    actions = [1,2,3]
-    policy = linucb.LinUCB(actions,h1,m1,alpha,d)
-    context = np.random.uniform(0,1,(T,d))
-    desired_action = np.zeros(shape = (T,1))
-    actual_action = np.zeros(shape = (T,1))
-    error = np.zeros(shape = (T,1))
-    for t in range(T):
-        if sum(context[t,:])<1:
-            desired_action[t] = 1
-        elif sum(context[t,:])<2.5:
-            desired_action[t] = 2
-        else:
-            desired_action[t] = 3
-        history_id, actual_action[t] = policy.get_action(context[t,:].tolist())
+    def __init__(self, d, actions):
+        self.HistoryStorage = history.MemoryHistoryStorage()
+        self.ModelStorage = model.MemoryModelStorage()
+        self.d = d
+        self.actions = actions
 
-        if actual_action[t] != desired_action[t]:
-            policy.reward(history_id,0)
-            error[t] = 1
-        else:
-            policy.reward(history_id,1)
-    return 1-sum(error)/T
-
-
-
-
-def test2(alpha,T):
-    # Nonlinear Payoff
-    h1 = history.MemoryHistoryStorage()
-    m1 = model.MemoryModelStorage()    
-    d = 5
-    actions = [1,2,3,4,5,6,7,8.9,10]
-    policy = linucb.LinUCB(actions,h1,m1,alpha,d)
-    context = np.random.uniform(0,2,(T,d))
-    desired_action = np.zeros(shape = (T,1))
-    actual_action = np.zeros(shape = (T,1))
-    error = np.zeros(shape = (T,1))
-    for t in range(T):
-        if sum(context[t,:])<2.5:
-            if context[t,1]<0.5:
+    def data_simulation(self, T, d):
+        context = np.random.uniform(0, 1, (T, d))
+        desired_action = np.zeros(shape = (T, 1))
+        for t in range(T):
+            if sum(context[t,:]) < 1:
                 desired_action[t] = 1
-            elif context[t,1]<1:
+            elif sum(context[t,:]) < 2.5:
                 desired_action[t] = 2
-            elif context[t,1]<1.5:
+            else:
                 desired_action[t] = 3
-            else:
-                desired_action[t] = 4
-        elif sum(context[t,:])<5:
-            if context[t,2]**2<1:
-                desired_action[t] = 4
-            elif context[t,2]**2<2:
-                desired_action[t] = 5
-            elif context[t,2]**2<3:
-                desired_action[t] = 6
-            else:
-                desired_action[t] = 7
+        return context, desired_action
+
+    def data_evaluation(self, policy, context, desired_action, alpha,d):
+        if policy != 'LinUCB':
+            print("We don't support other bandit algorithms now!")
         else:
-            if np.sin(context[t,3])>0.5:
-                desired_action[t] = 8
-            elif np.sin(context[t,3])>0:
-                desired_action[t] = 9
+            sum_error = 0
+            policy = linucb.LinUCB(self.actions, self.HistoryStorage, self.ModelStorage, alpha, d)
+            for t in range(T):
+                history_id, action = policy.get_action(context[t, :].tolist())
+            if desired_action[t] != action:
+                policy.reward(history_id, 0)
+                sum_error = sum_error + 1
             else:
-                desired_action[t] = 10
-        
-        history_id, actual_action[t] = policy.get_action(context[t,:].tolist())
-
-        if actual_action[t] != desired_action[t]:
-            policy.reward(history_id,0)
-            error[t] = 1
-        else:
-            policy.reward(history_id,1)
-            
-    return 1-sum(error)/T
-
-    
-# Plot test1 result
-test1_CTR = np.zeros(shape=(len(np.arange(0,3,0.1)),1))
-i = 0
-for alpha in np.arange(0,3,0.1):
-    test1_CTR[i] = test1(alpha,10000)
-    i = i + 1
-plt.plot(test1_CTR)
-
-# Plot test1 result
-test1_CTR = np.zeros(shape=(len(np.arange(0,3,0.1)),1))
-i = 0
-for alpha in np.arange(0,3,0.1):
-    test1_CTR[i] = test2(alpha,10000)
-    i = i + 1
-plt.plot(test1_CTR)
+                policy.reward(history_id, 1)
+        return 1 - sum_error / T
 

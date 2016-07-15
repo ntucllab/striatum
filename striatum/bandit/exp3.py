d1172737603aee1432e04fe1f0ef1cabfd1ce10a
David <tawei.huang1@gmail.com>
2016-07-15 12:13:28 +0800

    add Exp3

--- striatum/bandit/exp3.py
+++ striatum/bandit/exp3.py
@@ -1,45 +1,76 @@
 
 import logging
 
-import numpy as np
-
 from striatum.bandit.bandit import BaseBandit
+
+import numpy as np
 
 LOGGER = logging.getLogger(__name__)
 
 
 class Exp3(BaseBandit):
 
-    def __init__(self, actions, storage):
-        super(Exp3, self).__init__(storage, actions)
+    def __init__(self, actions, HistoryStorage, ModelStorage, gamma):
+        super(Exp3, self).__init__(HistoryStorage, ModelStorage, actions)
 
-        self.last_reward = None
-        self.last_history_id = None
+        self.last_history_id = -1
+        self.n_actions = len(self.actions)          # number of actions (i.e. K in the paper)
 
-        self.ucb1_ = self.ucb1()
+        # gamma in (0,1]
+        if not isinstance(gamma, float):
+            raise ValueError("gamma should be float, the one"
+                             "given is: %f" % gamma)
+        elif (gamma <= 0) or (gamma > 1):
+            raise ValueError("gamma should be in (0, 1], the one"
+                            "given is: %f" % gamma)
+        else:
+            self.gamma = gamma
 
-    def ucb1(self):
-        def upper_bound(t, n_plays):
-            return np.sqrt(2 * np.log(t) / n_plays)
+        # Initialize the model storage
+        query_vector = np.zeros(self.n_actions)     # probability distribution for action recommendation)
+        w = np.ones(self.n_actions)                 # weight vector
+        self._ModelStorage.save_model({'query_vector': query_vector, 'w': w})
 
-        t = 0
-        n_arms = len(self.actions)
-        n_plays = np.zeros(n_arms)
-        empirical_reward = np.zeros(n_arms)
+    def exp4p(self):
 
-        for i in range(n_arms):
-            reward = yield i
-            empirical_reward[i] += reward
-            t += 1
-            n_plays[i] += 1
+        """The generator which implements the main part of Exp3.
+        """
 
         while True:
-            ucbs = empirical_reward / n_plays + upper_bound(t, n_plays)
-            choice = np.argmax(ucbs)
-            reward = yield choice
-            empirical_reward[choice] += reward
-            n_plays[i] += 1
-            t += 1
+            context = yield
+
+            advice = np.zeros((self.n_experts, self.n_actions))
+            # get the expert advice (probability)
+            for i, model in enumerate(self.models):
+                if len(model.classes_) != len(self.actions):
+                    proba = model.predict_proba([context])
+                    k = 0
+                    for action in self.actions:
+                        if action in model.classes_:
+                            action_idx = self.actions.index(action)
+                            advice[i,action_idx] = proba[0][k]
+                            k = k + 1
+                        else:
+                            action_idx = self.actions.index(action)
+                            advice[i, action_idx] = self.pmin
+                else:
+                    advice[i, :] = model.predict_proba([context])
+
+            # choice vector, shape = (self.K, )
+            w = self._ModelStorage.get_model()['w']
+            w_sum = np.sum(w)
+            p_temp = (1 - self.n_actions * self.pmin) * w / w_sum + self.pmin
+
+            # query vector, shape= = (self.n_unlabeled, )
+            query_vector = np.dot(p_temp, advice)
+            self._ModelStorage.save_model({'query_vector': query_vector, 'w': w, 'advice': advice})
+
+            # give back the
+            action_idx = np.random.choice(np.arange(len(self.actions)), size=1, p = query_vector/sum(query_vector))[0]
+            action_max = self.actions[action_idx]
+            yield action_max
+
+        raise StopIteration
 
     def get_action(self, context):
         """Return the action to perform
@@ -47,7 +78,7 @@ class Exp3(BaseBandit):
         Parameters
         ----------
         context : {array-like, None}
-            The context of current state, None if no context avaliable.
+            The context of current state, None if no context available.
 
         Returns
         -------
@@ -57,19 +88,13 @@ class Exp3(BaseBandit):
         action : Actions object
             The action to perform.
         """
-        if context is not None:
-            LOGGER.warning("UCB1 does not support context.")
-
-        if self.last_reward is None:
-            raise ValueError("The last reward have not been passed in.")
-        action = self.ucb1_.send(self.last_reward)
-
-        self.last_reward = None
-
-        history_id = self.storage.add_history(None, action, reward=None)
-        self.last_history_id = history_id
-
-        return history_id, action
+        learn = self.exp4p()
+        learn.next()
+        action_max = learn.send(context)
+        self.n_total = self.n_total + 1
+        self.last_history_id = self.last_history_id + 1
+        self._HistoryStorage.add_history(np.transpose(np.array([context])), action_max, reward=None)
+        return self.last_history_id, action_max
 
     def reward(self, history_id, reward):
         """Reward the preivous action with reward.
@@ -83,7 +108,31 @@ class Exp3(BaseBandit):
             A float representing the feedback given to the action, the higher
             the better.
         """
-        if history_id != self.last_history_id:
-            raise ValueError("The history_id should be the same as last one.")
-        self.last_reward = reward
-        self.storage.reward(history_id, reward)
+
+        reward_action = self._HistoryStorage.unrewarded_histories[history_id].action
+        reward_action_idx = self.actions.index(reward_action)
+        w_old = self._ModelStorage.get_model()['w']
+        query_vector = self._ModelStorage.get_model()['query_vector']
+        advice = self._ModelStorage.get_model()['advice']
+
+        # Update the model
+        rhat = np.zeros(self.n_actions)
+        rhat[reward_action_idx] = reward/query_vector[reward_action_idx]
+        yhat = np.dot(advice, rhat)
+        vhat = np.zeros(self.n_experts)
+        for i in range(self.n_experts):
+            for j in range(self.n_actions):
+                vhat[i] = vhat[i] + advice[i,j]/query_vector[j]
+
+        w_new = w_old * np.exp(
+                           self.pmin / 2 * (
+                                yhat + vhat * np.sqrt(
+                                    np.log(self.n_experts / self.delta) / self.n_actions / self.n_total
+                                )
+                            )
+                        )
+
+        self._ModelStorage.save_model({'query_vector': query_vector, 'w': w_new, 'advice': advice})
+
+        # Update the history
+        self._HistoryStorage.add_reward(history_id, reward)

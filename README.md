3e59dcb13f70c573ee050276f3acda888d6bc4fb
cph <stegben.benjamin@gmail.com>
2016-07-26 12:37:15 +0800

    add nosetest cfg, add init in bandit.tests

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Striatum
 Contextual bandit in python
+
+
+## Test
+```
+python setup.py nosetests
+```
--- setup.py
+++ setup.py
@@ -8,10 +8,13 @@ setup(
     author='test',
     author_email='test@example.com',
     url='https://github.com/ntucllab/straitum',
+    setup_requires=[
+        'nose>=1.0',
+    ],
     classifiers=[
-        "Topic :: Scientific/Engineering",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3",
+        'Topic :: Scientific/Engineering',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
     ],
     test_suite='striatum',
     packages=[

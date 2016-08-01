456f3ad2c81a17e0f8ed6fd9f176868cf4bc3a2e
David <tawei.huang1@gmail.com>
2016-08-01 11:40:44 +0800

    add exp3 and random; finish examples

--- examples/movielens_bandit.py
+++ examples/movielens_bandit.py
@@ -1,8 +1,7 @@
 """
 The script uses real-world data to conduct contextual bandit experiments. Here we use
-MovieLens 10M Dataset, which is released by GroupLens at 1/2009. Please fist download
-the dataset from http://grouplens.org/datasets/movielens/, then unzipped the file
-"ml-1m.zip" to the examples folder.
+MovieLens 10M Dataset, which is released by GroupLens at 1/2009. Please fist pre-process
+datasets (use "movielens_preprocess.py"), and then you can run this example.
 """
 
 import pandas as pd
@@ -14,9 +13,11 @@ from striatum.bandit import ucb1
 from striatum.bandit import linucb
 from striatum.bandit import linthompsamp
 from striatum.bandit import exp4p
+from striatum.bandit import exp3
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.linear_model import LogisticRegression
 from sklearn.multiclass import OneVsRestClassifier
+
 
 def get_data():
     streaming_batch = pd.read_csv('streaming_batch.csv', sep='\t', names=['user_id'], engine='c')
@@ -43,23 +44,29 @@ def policy_generation(bandit, action_context, actions):
         models = expert_training(action_context)
         policy = exp4p.Exp4P(actions, historystorage, modelstorage, models, delta=0.5, pmin=None)
 
-    if bandit == 'LinUCB':
+    elif bandit == 'LinUCB':
         policy = linucb.LinUCB(actions, historystorage, modelstorage, 0.5, 20)
 
-    if bandit == 'LinThompSamp':
+    elif bandit == 'LinThompSamp':
         policy = linthompsamp.LinThompSamp(actions, historystorage, modelstorage,
                                            d=20, delta=0.9, r=0.01, epsilon=0.5)
 
-    if bandit == 'UCB1':
+    elif bandit == 'UCB1':
         policy = ucb1.UCB1(actions, historystorage, modelstorage)
+
+    elif bandit == 'Exp3':
+        policy = exp3.Exp3(actions, historystorage, modelstorage, gamma=0.2)
+
+    elif bandit == 'random':
+        policy = 0
 
     return policy
 
 
-def policy_evaluation(policy, streaming_batch, user_feature, reward_list):
+def policy_evaluation(policy, bandit, streaming_batch, user_feature, reward_list, actions):
     times = len(streaming_batch)
     seq_error = np.zeros(shape=(times, 1))
-    if bandit in ['LinUCB', 'LinThompSamp', 'UCB1']:
+    if bandit in ['LinUCB', 'LinThompSamp', 'UCB1', 'Exp3']:
 
         for t in range(times):
             feature = user_feature[user_feature.index == streaming_batch.iloc[t, 0]]
@@ -96,6 +103,22 @@ def policy_evaluation(policy, streaming_batch, user_feature, reward_list):
                 policy.reward(history_id, 1)
                 if t > 0:
                     seq_error[t] = seq_error[t - 1]
+
+    elif bandit == 'random':
+        for t in range(times):
+            action = actions[np.random.randint(0, len(actions)-1)]
+            watched_list = reward_list[reward_list['user_id'] == streaming_batch.iloc[t, 0]]
+
+            if action not in list(watched_list['movie_id']):
+                if t == 0:
+                    seq_error[t] = 1.0
+                else:
+                    seq_error[t] = seq_error[t - 1] + 1.0
+
+            else:
+                if t > 0:
+                    seq_error[t] = seq_error[t - 1]
+
     return seq_error
 
 
@@ -105,26 +128,30 @@ def regret_calculation(seq_error):
     return regret
 
 
-if __name__ == '__main__':
-
+def main():
     streaming_batch, user_feature, actions, reward_list, action_context = get_data()
-    streaming_batch_small = streaming_batch.iloc[0:1000]
+    streaming_batch_small = streaming_batch.iloc[0:10000]
 
-    # conduct regret analyses for LinUCB and LinThompSamp
-    experiment_bandit = ['LinUCB', 'LinThompSamp', 'Exp4P', 'UCB1']
+    # conduct regret analyses
+    experiment_bandit = ['LinUCB', 'LinThompSamp', 'Exp4P', 'UCB1', 'Exp3', 'random']
     regret = {}
     col = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'w']
     i = 0
     for bandit in experiment_bandit:
         policy = policy_generation(bandit, action_context, actions)
-        seq_error = policy_evaluation(policy, streaming_batch_small, user_feature, reward_list)
+        seq_error = policy_evaluation(policy, bandit, streaming_batch_small, user_feature, reward_list, actions)
         regret[bandit] = regret_calculation(seq_error)
-        plt.plot(range(1000), regret[bandit], c= col[i], ls='-', marker='.', label=bandit)
+        plt.plot(range(len(streaming_batch_small)), regret[bandit], c=col[i], ls='-',
+                 marker='.', label=bandit)
         plt.xlabel('time')
         plt.ylabel('regret')
-        plt.legend()
+        plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
         axes = plt.gca()
         axes.set_ylim([0, 1])
         plt.title("Regret Bound with respect to T")
         i += 1
     plt.show()
+
+
+if __name__ == '__main__':
+    main()

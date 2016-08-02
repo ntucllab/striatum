df795533e34807c67106cbddcac893e3f0d30df7
David <tawei.huang1@gmail.com>
2016-08-02 13:35:25 +0800

    refactor simulation exp3.py

--- simulation/simulation_exp3.py
+++ simulation/simulation_exp3.py
@@ -1,90 +1,61 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import sys
-sys.path.append("..")
 from striatum.storage import history
 from striatum.storage import model
 from striatum.bandit import exp3
+import simulation as sm
+import numpy as np
+import matplotlib.pyplot as plt
 
 
-class Exp3LinearPayoff:
+def main():
+    times = 1000
+    d = 5
+    actions = [1, 2, 3, 4, 5]
 
-    def __init__(self, t, d, actions):
-        self.t = t
-        self.d = d
-        self.actions = actions
+    # Parameter tunning
+    tunning_region = np.arange(0.001, 1, 0.03)
+    ctr_tunning = np.zeros(shape=(len(tunning_region), 1))
+    context1, desired_action1 = sm.data_simulation(times, d, actions)
+    i = 0
+    for gamma in tunning_region:
+        historystorage = history.MemoryHistoryStorage()
+        modelstorage = model.MemoryModelStorage()
+        policy = exp3.Exp3(actions, historystorage, modelstorage, gamma)
+        seq_error = sm.policy_evaluation(policy, context1, desired_action1)
+        ctr_tunning[i] = times - seq_error[-1]
+        i += 1
+    ctr_tunning /= times
+    gamma_opt = tunning_region[np.argmax(ctr_tunning)]
 
-    def data_simulation(self):
-        context = np.random.uniform(0, 1, (self.t, self.d))
-        desired_action = np.zeros(shape=(self.t, 1))
-        n_actions = len(self.actions)
-        for t in range(self.t):
-            for i in range(n_actions):
-                if i * self.d/n_actions < sum(context[t, :]) <= (i+1) * self.d/n_actions:
-                    desired_action[t] = self.actions[i]
-        return context, desired_action
+    # Plot the parameter tunning result
+    plt.plot(tunning_region, ctr_tunning, 'ro-', label="gamma changes")
+    plt.xlabel('parameter value')
+    plt.ylabel('CTR')
+    plt.legend()
+    axes = plt.gca()
+    axes.set_ylim([0, 1])
+    plt.title("Parameter Tunning Curve - EXP3")
+    plt.show()
 
-    def policy_evaluation(self, policy, context, desired_action, gamma):
-        if policy != 'EXP3':
-            print("We don't support other bandit algorithms now!")
-        else:
-            historystorage = history.MemoryHistoryStorage()
-            modelstorage = model.MemoryModelStorage()
-            seq_error = np.zeros(shape=(self.t, 1))
-            policy = exp3.Exp3(self.actions, historystorage, modelstorage, gamma)
-            for t in range(self.t):
-                history_id, action = policy.get_action(context[t])
-                if desired_action[t][0] != action:
-                    policy.reward(history_id, 0)
-                    # sum_error += 1
-                    if t == 0:
-                        seq_error[t] = 1.0
-                    else:
-                        seq_error[t] = seq_error[t - 1] + 1.0
-                else:
-                    policy.reward(history_id, 1)
-                    if t > 0:
-                        seq_error[t] = seq_error[t - 1]
-            return seq_error
+    # Regret Analysis
+    times = 10000
+    context2, desired_action2 = sm.data_simulation(times, d, actions)
+    historystorage = history.MemoryHistoryStorage()
+    modelstorage = model.MemoryModelStorage()
+    policy = exp3.Exp3(actions, historystorage, modelstorage, gamma=gamma_opt)
 
-    def parameter_tuning(self):
-        tunning_region = np.arange(0.001, 1, 0.03)
-        ctr = np.zeros(shape=(len(tunning_region), 1))
-        context, desired_action = self.data_simulation()
-        i = 0
-        for gamma in tunning_region:
-            seq_error = self.policy_evaluation('EXP3', context, desired_action, gamma)
-            ctr[i] = self.t - seq_error[-1]
-            i += 1
-        ctr = ctr / self.t
-        plt.figure(1)
-        plt.subplot(211)
-        plt.plot(tunning_region, ctr, 'ro-', label="gamma changes")
-        plt.xlabel('parameter value')
-        plt.ylabel('CTR')
-        plt.legend()
-        axes = plt.gca()
-        axes.set_ylim([0, 1])
-        plt.title("Parameter Tunning Curve - EXP3")
-        plt.show()
+    seq_error = sm.policy_evaluation(policy, context2, desired_action2)
+    seq_error = [x / y for x, y in zip(seq_error, range(1, times + 1))]
 
-    def regret_bound(self):
-        context, desired_action = self.data_simulation()
-        seq_error = self.policy_evaluation('EXP3', context, desired_action, gamma=0.18)
-        seq_error = [x/y for x, y in zip(seq_error, range(1, self.t + 1))]
-        plt.figure(1)
-        plt.subplot(211)
-        plt.plot(range(self.t), seq_error, 'r-', label="gamma=0.18")
-        plt.xlabel('time')
-        plt.ylabel('regret')
-        plt.legend()
-        axes = plt.gca()
-        axes.set_ylim([0, 1])
-        plt.title("Regret Bound with respect to T - EXP3")
-        plt.show()
+    # Plot the regret analysis
+    plt.plot(range(times), seq_error, 'r-', label='gamma = ' + str(gamma_opt))
+    plt.xlabel('time')
+    plt.ylabel('regret')
+    plt.legend()
+    axes = plt.gca()
+    axes.set_ylim([0, 1])
+    plt.title("Regret Bound with respect to T - EXP3")
+    plt.show()
+
 
 if __name__ == '__main__':
-    simulation = Exp3LinearPayoff(1000, 5, [1, 2, 3, 4, 5])
-    simulation.parameter_tuning()
-    simulation2 = Exp3LinearPayoff(10000, 5, [1, 2, 3, 4, 5])
-    simulation2.regret_bound()
+    main()

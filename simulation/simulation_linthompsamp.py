93ae3054518de09be49d77041df3703e3f4ec204
David <tawei.huang1@gmail.com>
2016-08-09 14:42:16 +0800

    update simulation

--- simulation/simulation_linthompsamp.py
+++ simulation/simulation_linthompsamp.py
@@ -5,12 +5,18 @@ from striatum import simulation
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
+from striatum.bandit.bandit import Action
 
 
 def main():
     times = 1000
     d = 5
-    actions = [1, 2, 3, 4, 5]
+    a1 = Action(1, 'a1', 'content 1')
+    a2 = Action(2, 'a2', 'content 2')
+    a3 = Action(3, 'a3', 'content 3')
+    a4 = Action(4, 'a4', 'content 4')
+    a5 = Action(5, 'a5', 'content 5')
+    actions = [a1, a2, a3, a4, a5]
 
     # Parameter tunning
     tunning_region = np.arange(0.01, 0.99, 0.1)

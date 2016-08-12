afd9523182bf07bf85836f0a095455ca6a0412fd
David <tawei.huang1@gmail.com>
2016-08-12 10:34:46 +0800

    finish ucb1

--- striatum/bandit/tests/test_ucb1.py
+++ striatum/bandit/tests/test_ucb1.py
@@ -4,13 +4,19 @@ sys.path.append("..")
 from striatum.storage import history as history
 from striatum.storage import model as model
 from striatum.bandit import ucb1
+from striatum.bandit.bandit import Action
 
 
 class Ucb1(unittest.TestCase):
     def setUp(self):
         self.modelstorage = model.MemoryModelStorage()
         self.historystorage = history.MemoryHistoryStorage()
-        self.actions = [1, 2, 3, 4, 5]
+        a1 = Action(1, 'a1', 'i love u')
+        a2 = Action(2, 'a2', 'i hate u')
+        a3 = Action(3, 'a3', 'i do not understand')
+        a4 = Action(4, 'a4', 'i love u very much')
+        a5 = Action(5, 'a5', 'i hate u very nuch')
+        self.actions = [a1, a2, a3, a4, a5]
         self.alpha = 1.00
 
     def test_initialization(self):
@@ -19,39 +25,39 @@ class Ucb1(unittest.TestCase):
 
     def test_get_first_action(self):
         policy = ucb1.UCB1(self.actions, self.historystorage, self.modelstorage)
-        history_id, action = policy.get_action(context=None)
+        history_id, action = policy.get_action(context=None, n_action=1)
         self.assertEqual(history_id, 0)
-        self.assertIn(action, self.actions)
+        self.assertIn(action[0]['action'], self.actions)
 
     def test_update_reward(self):
         policy = ucb1.UCB1(self.actions, self.historystorage, self.modelstorage)
-        history_id, action = policy.get_action(context=None)
-        policy.reward(history_id, 1)
-        self.assertEqual(policy._historystorage.get_history(history_id).reward, 1)
+        history_id, action = policy.get_action(context=None, n_action=1)
+        policy.reward(history_id, {1: 0})
+        self.assertEqual(policy._historystorage.get_history(history_id).reward, {1: 0})
 
     def test_model_storage(self):
         policy = ucb1.UCB1(self.actions, self.historystorage, self.modelstorage)
-        history_id, action = policy.get_action(context=None)
-        policy.reward(history_id, 1)
-        self.assertEqual(policy._modelstorage._model['empirical_reward'][action], 2)
-        self.assertEqual(policy._modelstorage._model['n_actions'][action], 2.0)
+        history_id, action = policy.get_action(context=None, n_action=1)
+        policy.reward(history_id, {action[0]['action'].action_id: 1.0})
+        self.assertEqual(policy._modelstorage._model['empirical_reward'][action[0]['action'].action_id], 2)
+        self.assertEqual(policy._modelstorage._model['n_actions'][action[0]['action'].action_id], 2.0)
         self.assertEqual(policy._modelstorage._model['n_total'], 6.0)
 
     def test_delay_reward(self):
         policy = ucb1.UCB1(self.actions, self.historystorage, self.modelstorage)
-        history_id, action = policy.get_action(context=None)
-        history_id_2, action_2 = policy.get_action(context=None)
-        policy.reward(history_id, 1)
-        self.assertEqual(policy._historystorage.get_history(history_id).reward, 1.0)
+        history_id, action = policy.get_action(context=None, n_action=1)
+        history_id_2, action_2 = policy.get_action(context=None, n_action=1)
+        policy.reward(history_id, {1: 0})
+        self.assertEqual(policy._historystorage.get_history(history_id).reward, {1: 0})
         self.assertEqual(policy._historystorage.get_history(history_id_2).reward, None)
 
     def test_reward_order_descending(self):
         policy = ucb1.UCB1(self.actions, self.historystorage, self.modelstorage)
-        history_id, action = policy.get_action(context=None)
-        history_id_2, action_2 = policy.get_action(context=None)
-        policy.reward(history_id_2, 1)
+        history_id, action = policy.get_action(context=None, n_action=1)
+        history_id_2, action_2 = policy.get_action(context=None, n_action=2)
+        policy.reward(history_id_2, {1: 0, 2: 0})
         self.assertEqual(policy._historystorage.get_history(history_id).reward, None)
-        self.assertEqual(policy._historystorage.get_history(history_id_2).reward, 1.0)
+        self.assertEqual(policy._historystorage.get_history(history_id_2).reward, {1: 0, 2: 0})
 
 
 if __name__ == '__main__':

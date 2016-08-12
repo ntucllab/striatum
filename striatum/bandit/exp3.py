324b91bf6d7e569c91c6e1cfb68b8307d627344f
David <tawei.huang1@gmail.com>
2016-08-12 12:06:22 +0800

    update exp3

--- striatum/bandit/exp3.py
+++ striatum/bandit/exp3.py
@@ -41,7 +41,6 @@ class Exp3(BaseBandit):
     def __init__(self, actions, historystorage, modelstorage, gamma):
         super(Exp3, self).__init__(historystorage, modelstorage, actions)
 
-        self.n_actions = len(self._actions)  # number of actions (i.e. K in the paper)
         self.exp3_ = None
 
         # gamma in (0,1]
@@ -55,8 +54,11 @@ class Exp3(BaseBandit):
             self.gamma = gamma
 
         # Initialize the model storage
-        query_vector = np.zeros(self.n_actions)  # probability distribution for action recommendation)
-        w = np.ones(self.n_actions)  # weight vector
+        query_vector = {}
+        w = {}
+        for action_id in self._actions_id:
+            query_vector[action_id] = 0     # probability distribution for action recommendation)
+            w[action_id] = 1                # weight vector
         self._modelstorage.save_model({'query_vector': query_vector, 'w': w})
 
     def exp3(self):
@@ -66,68 +68,107 @@ class Exp3(BaseBandit):
 
         while True:
             w = self._modelstorage.get_model()['w']
-            w_sum = np.sum(w)
-            query_vector = (1 - self.gamma) * w / w_sum + self.gamma / self.n_actions
+            w_sum = np.sum(w.values())
+
+            for action_id in self._actions_id:
+                query_vector = (1 - self.gamma) * w[action_id] / w_sum + self.gamma / len(self._actions_id)
 
             self._modelstorage.save_model({'query_vector': query_vector, 'w': w})
 
-            action_idx = np.random.choice(np.arange(len(self._actions)), size=1, p=query_vector / sum(query_vector))[0]
-            action_max = self._actions[action_idx]
-            yield action_max
+            estimated_reward = {}
+            uncertainty = {}
+            score = {}
+            for i in range(self.k):
+                estimated_reward[self._actions_id[i]] = query_vector[i]
+                uncertainty[self._actions_id[i]] = 0
+                score[self._actions_id[i]] = query_vector[i]
+            yield estimated_reward, uncertainty, score
 
         raise StopIteration
 
-    def get_action(self, context):
-
+    def get_action(self, context, n_action=1):
         """Return the action to perform
 
-        Parameters
-        ----------
-        context : {array-like, None}
-            The context of current state, None if no context available.
+            Parameters
+            ----------
+            context : {array-like, None}
+                The context of current state, None if no context available.
 
-        Returns
-        -------
-        history_id : int
-            The history id of the action.
+            n_action: int
+                Number of actions wanted to recommend users.
 
-        action : Actions object
-            The action to perform.
+            Returns
+            -------
+            history_id : int
+                The history id of the action.
+
+            action : list of dictionaries
+                In each dictionary, it will contains {rank: Action object, estimated_reward, uncertainty}
         """
 
         if self.exp3_ is None:
             self.exp3_ = self.exp3()
-            action_max = six.next(self.exp3_)
+            estimated_reward, uncertainty, score = six.next(self.exp3_)
         else:
-            action_max = six.next(self.exp3_)
+            estimated_reward, uncertainty, score = six.next(self.exp3_)
 
-        history_id = self._historystorage.add_history(np.transpose(np.array([context])), action_max, reward=None)
-        return history_id, action_max
+        action_recommend = []
+        actions_recommend_id = np.random.choice(self._actions_id, size=n_action, p=score.values(), replace=False)
+
+        for action_id in actions_recommend_id:
+            action_id = int(action_id)
+            action = [action for action in self._actions if action.action_id == action_id][0]
+            action_recommend.append({'action': action, 'estimated_reward': estimated_reward[action_id],
+                                     'uncertainty': uncertainty[action_id], 'score': score[action_id]})
+
+        history_id = self._historystorage.add_history(context, action_recommend, reward=None)
+        return history_id, action_recommend
 
     def reward(self, history_id, reward):
-        """Reward the preivous action with reward.
+        """Reward the previous action with reward.
 
-        Parameters
-        ----------
-        history_id : int
-            The history id of the action to reward.
+            Parameters
+            ----------
+            history_id : int
+                The history id of the action to reward.
 
-        reward : float
-            A float representing the feedback given to the action, the higher
-            the better.
+            reward : dictionary
+                The dictionary {action_id, reward}, where reward is a float.
         """
 
-        reward_action = self._historystorage.unrewarded_histories[history_id].action
-        reward_action_idx = self._actions.index(reward_action)
-        w_old = self._modelstorage.get_model()['w']
+        w = self._modelstorage.get_model()['w']
         query_vector = self._modelstorage.get_model()['query_vector']
+        actions_id = query_vector.keys()
 
         # Update the model
-        rhat = np.zeros(self.n_actions)
-        rhat[reward_action_idx] = reward / query_vector[reward_action_idx]
-        w_new = w_old * np.exp(self.gamma * rhat / self.n_actions)
+        for action_id, reward_tmp in reward.items():
+            rhat = {}
+            for i in actions_id:
+                rhat[i] = 0.0
+            rhat[action_id] = reward_tmp / query_vector[action_id]
+            w[action_id] *= np.exp(self.gamma * rhat / len(self._actions_id))
 
-        self._modelstorage.save_model({'query_vector': query_vector, 'w': w_new})
+        self._modelstorage.save_model({'query_vector': query_vector, 'w': w})
 
         # Update the history
         self._historystorage.add_reward(history_id, reward)
+
+    def add_action(self, actions):
+        """ Add new actions (if needed).
+
+            Parameters
+            ----------
+            actions : list
+                Actions (arms) for recommendation
+        """
+
+        actions_id = [actions[i].action_id for i in range(len(actions))]
+        w = self._modelstorage.get_model()['w']
+        query_vector = self._modelstorage.get_model()['query_vector']
+
+        for action_id in actions_id:
+            query_vector[action_id] = 0
+            w[action_id] = 1  # weight vector
+
+        self._actions.extend(actions)
+        self._actions_id.extend(actions_id)

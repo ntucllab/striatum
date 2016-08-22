28bb96f0cd2dde869b2b3ee37d70b8b6add23fc5
David <tawei.huang1@gmail.com>
2016-08-22 12:12:45 +0800

    refine function name and delete unnecessary function

--- striatum/simulation.py
+++ striatum/simulation.py
@@ -2,9 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def data_simulation(times, d, actions, algorithm=None):
+def simulate_data(times, d, actions, algorithm=None):
 
-    """Simulate dataset for linucb and linthompsamp algorithms.
+    """Simulate dataset for the contextual bandit problem.
 
         Parameters
         ----------
@@ -49,7 +49,7 @@ def data_simulation(times, d, actions, algorithm=None):
     return context, desired_action
 
 
-def policy_evaluation(policy, context, desired_action):
+def evaluate_policy(policy, context, desired_action):
 
     """Evaluate a given policy based on a (context, desired_action) dataset.
 
@@ -67,51 +67,29 @@ def policy_evaluation(policy, context, desired_action):
         Return
         ---------
 
-        seq_error: array
+        cum_regret: array
             The cumulative regret at each iteration.
 
     """
 
     times = len(desired_action)
-    seq_error = np.zeros(shape=(times, 1))
+    cum_regret = np.zeros(shape=(times, 1))
     for t in range(times):
         history_id, action = policy.get_action(context[t], 1)
         if desired_action[t][0] != action[0]['action'].action_id:
             policy.reward(history_id, {action[0]['action'].action_id: 0})
             if t == 0:
-                seq_error[t] = 1.0
+                cum_regret[t] = 1.0
             else:
-                seq_error[t] = seq_error[t - 1] + 1.0
+                cum_regret[t] = cum_regret[t - 1] + 1.0
         else:
             policy.reward(history_id, {action[0]['action'].action_id: 1})
             if t > 0:
-                seq_error[t] = seq_error[t - 1]
-    return seq_error
+                cum_regret[t] = cum_regret[t - 1]
+    return cum_regret
 
 
-def regret_calculation(seq_error):
-
-    """Evaluate a given policy based on a (context, desired_action) dataset.
-
-        Parameters
-        ----------
-        seq_error: array
-            The cumulative regret at each iteration.
-
-        Return
-        ---------
-
-        regret: array
-            The average cumulative regret at each iteration.
-
-    """
-
-    t = len(seq_error)
-    regret = [x / y for x, y in zip(seq_error, range(1, t + 1))]
-    return regret
-
-
-def tuning_plot(tuning_region, ctr_tuning, label):
+def plot_tuning_curve(tuning_region, ctr_tuning, label):
 
     """Draw the parameter tuning plot
 
@@ -135,30 +113,4 @@ def tuning_plot(tuning_region, ctr_tuning, label):
     axes = plt.gca()
     axes.set_ylim([0, 1])
     plt.title("Parameter Tunning Curve")
-    plt.show()
-
-
-def regret_plot(times, regret, label):
-
-    """Draw the parameter tuning plot
-
-        Parameters
-        ----------
-        times: int
-            Total number of (context, reward) tuples you want to generate.
-
-        regret: array
-            The average cumulative regret at each iteration.
-
-        label: string
-            The name of label want to show.
-    """
-
-    plt.plot(range(times), regret, 'r-', label=label)
-    plt.xlabel('time')
-    plt.ylabel('regret')
-    plt.legend()
-    axes = plt.gca()
-    axes.set_ylim([0, 1])
-    plt.title("Regret Bound with respect to T")
     plt.show()

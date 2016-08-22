22d029b62a24e994e8bc1bde88f51f23c522085f
David <tawei.huang1@gmail.com>
2016-08-22 11:39:30 +0800

    finish rewardplot

--- striatum/rewardplot.py
+++ striatum/rewardplot.py
@@ -3,23 +3,26 @@ import matplotlib.pyplot as plt
 
 
 def calculate_cum_reward(policy, last_history_id):
-    cum_reward = {0: 0.0}
-    for i in range(1, last_history_id + 1):
-        cum_reward[i] = cum_reward[i - 1] + policy.historystorage.get_history(i).reward
-    return cum_reward
+    cum_reward = {-1: 0.0}
+    time = {-1: 0.0}
+    for i in range(0, last_history_id + 1):
+        reward = policy.historystorage.get_history(i).reward
+        time[i] = time[i - 1] + len(reward.values())
+        cum_reward[i] = cum_reward[i - 1] + sum(reward.values())
+    return cum_reward, time
 
 
 def calculate_avg_reward(policy, last_history_id):
-    cum_reward = calculate_cum_reward(policy, last_history_id)
+    cum_reward, time = calculate_cum_reward(policy, last_history_id)
     avg_reward = {}
-    for i in range(1, last_history_id + 1):
-        avg_reward[i] = cum_reward[i] / i
+    for i in range(0, last_history_id + 1):
+        avg_reward[i] = cum_reward[i] / time[i]
     return avg_reward
 
 
 def plot_avg_reward(policy, last_history_id):
     avg_reward = calculate_avg_reward(policy, last_history_id)
-    plt.plot(avg_reward.keys, avg_reward.values(), 'r-', label="average reward")
+    plt.plot(avg_reward.keys(), avg_reward.values(), 'r-', label="average reward")
     plt.xlabel('time')
     plt.ylabel('avg reward')
     plt.legend()

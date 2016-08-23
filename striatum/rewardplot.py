7c78816b7027bb1463ef335ea21decec227f516e
David <tawei.huang1@gmail.com>
2016-08-23 17:04:38 +0800

    modify plot method

--- striatum/rewardplot.py
+++ striatum/rewardplot.py
@@ -1,17 +1,14 @@
 import matplotlib.pyplot as plt
 
 
-def calculate_cum_reward(policy, last_history_id):
+def calculate_cum_reward(policy):
 
-    """Simulate dataset for linucb and linthompsamp algorithms.
+    """Calculate cumulative reward with respect to time.
 
         Parameters
         ----------
         policy: bandit object
             The bandit algorithm you want to evaluate.
-
-        last_history_id: int
-            The latest history_id for your evaluation time period.
 
         Return
         ---------
@@ -22,26 +19,24 @@ def calculate_cum_reward(policy, last_history_id):
             The dictionary stores {history_id: cumulative number of recommended actions} .
     """
 
+    last_history_id = policy.historystorage.n_histories
     cum_reward = {-1: 0.0}
     time = {-1: 0.0}
-    for i in range(0, last_history_id + 1):
+    for i in range(0, last_history_id):
         reward = policy.historystorage.get_history(i).reward
         time[i] = time[i - 1] + len(reward.values())
         cum_reward[i] = cum_reward[i - 1] + sum(reward.values())
     return cum_reward, time
 
 
-def calculate_avg_reward(policy, last_history_id):
+def calculate_avg_reward(policy):
 
-    """Simulate dataset for linucb and linthompsamp algorithms.
+    """Calculate average reward with respect to time.
 
         Parameters
         ----------
         policy: bandit object
             The bandit algorithm you want to evaluate.
-
-        last_history_id: int
-            The latest history_id for your evaluation time period.
 
         Return
         ---------
@@ -49,14 +44,15 @@ def calculate_avg_reward(policy, last_history_id):
             The dictionary stores {history_id: average reward} .
     """
 
-    cum_reward, time = calculate_cum_reward(policy, last_history_id)
+    last_history_id = policy.historystorage.n_histories
+    cum_reward, time = calculate_cum_reward(policy)
     avg_reward = {}
-    for i in range(0, last_history_id + 1):
+    for i in range(0, last_history_id):
         avg_reward[i] = cum_reward[i] / time[i]
     return avg_reward
 
 
-def plot_avg_reward(policy, last_history_id):
+def plot_avg_reward(policy):
 
     """Plot average reward with respect to time.
 
@@ -64,12 +60,9 @@ def plot_avg_reward(policy, last_history_id):
         ----------
         policy: bandit object
             The bandit algorithm you want to evaluate.
-
-        last_history_id: int
-            The latest history_id for your evaluation time period.
     """
 
-    avg_reward = calculate_avg_reward(policy, last_history_id)
+    avg_reward = calculate_avg_reward(policy)
     plt.plot(avg_reward.keys(), avg_reward.values(), 'r-', label="average reward")
     plt.xlabel('time')
     plt.ylabel('avg reward')
@@ -78,7 +71,7 @@ def plot_avg_reward(policy, last_history_id):
     plt.show()
 
 
-def plot_avg_regret(policy, last_history_id):
+def plot_avg_regret(policy):
 
     """Plot average regret with respect to time.
 
@@ -86,12 +79,9 @@ def plot_avg_regret(policy, last_history_id):
         ----------
         policy: bandit object
             The bandit algorithm you want to evaluate.
-
-        last_history_id: int
-            The latest history_id for your evaluation time period.
     """
 
-    avg_reward = calculate_avg_reward(policy, last_history_id)
+    avg_reward = calculate_avg_reward(policy)
     plt.plot(avg_reward.keys(), [1.0 - reward for reward in avg_reward.values()], 'r-', label="average regret")
     plt.xlabel('time')
     plt.ylabel('avg regret')

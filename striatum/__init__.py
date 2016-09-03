f756539b40f29824fea08c4e3f94aa4350c3cfac
yangarbiter <yangarbiter@gmail.com>
2016-09-03 13:47:48 +0800

    import modules in __init__ and add docstrings

--- striatum/__init__.py
+++ striatum/__init__.py
@@ -1,1 +1,15 @@
- 
+"""
+striatum
+========
+
+Available subpackages
+---------------------
+bandit
+    Implementation of bandit algorithms.
+storage
+    Interface for storing history.
+
+"""
+
+
+__all__ = ['bandit', 'storage']

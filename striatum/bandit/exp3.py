c4a1bf0f54c3516268329488f473aa4196a23868
yangarbiter <yangarbiter@gmail.com>
2016-09-06 09:59:32 +0800

    better exp3 coding style

--- striatum/bandit/exp3.py
+++ striatum/bandit/exp3.py
@@ -1,20 +1,20 @@
 """ Exp3: Exponential-weight algorithm for Exploration and Exploitation
-This module contains a class that implements EXP3, a bandit algorithm that randomly choose an action
-according to a learned probability distribution.
+This module contains a class that implements EXP3, a bandit algorithm that
+randomly choose an action according to a learned probability distribution.
 """
 
 import logging
-import six
-from striatum.bandit.bandit import BaseBandit
 
 import numpy as np
+import six
+
+from striatum.bandit.bandit import BaseBandit
 
 LOGGER = logging.getLogger(__name__)
 
 
 class Exp3(BaseBandit):
-
-    """Exp3 algorithm.
+    r"""Exp3 algorithm.
 
     Parameters
     ----------
@@ -28,7 +28,8 @@ class Exp3(BaseBandit):
         The place where we store the model parameters.
 
     gamma: float, 0 < gamma <= 1
-        The parameter used to control the minimum chosen probability for each action.
+        The parameter used to control the minimum chosen probability for each
+        action.
 
     Attributes
     ----------
@@ -37,8 +38,8 @@ class Exp3(BaseBandit):
 
     References
     ----------
-    .. [1]  Peter Auer, Nicolo Cesa-Bianchi, et al. "The non-stochastic multi-armed bandit problem ."
-            SIAM Journal of Computing. 2002.
+    .. [1]  Peter Auer, Nicolo Cesa-Bianchi, et al. "The non-stochastic
+            multi-armed bandit problem ." SIAM Journal of Computing. 2002.
     """
 
     def __init__(self, actions, historystorage, modelstorage, gamma):
@@ -60,8 +61,10 @@ class Exp3(BaseBandit):
         query_vector = {}
         w = {}
         for action_id in self.action_ids:
-            query_vector[action_id] = 0     # probability distribution for action recommendation)
-            w[action_id] = 1                # weight vector
+            # probability distribution for action recommendation)
+            query_vector[action_id] = 0
+            # weight vector
+            w[action_id] = 1
         self._modelstorage.save_model({'query_vector': query_vector, 'w': w})
 
     def exp3(self):
@@ -74,9 +77,11 @@ class Exp3(BaseBandit):
 
             query_vector = {}
             for action_id in self.action_ids:
-                query_vector[action_id] = (1 - self.gamma) * w[action_id] / w_sum + self.gamma / len(self.action_ids)
+                query_vector[action_id] = (1 - self.gamma) *\
+                    w[action_id] / w_sum + self.gamma / len(self.action_ids)
 
-            self._modelstorage.save_model({'query_vector': query_vector, 'w': w})
+            self._modelstorage.save_model(
+                {'query_vector': query_vector, 'w': w})
 
             estimated_reward = {}
             uncertainty = {}
@@ -107,7 +112,8 @@ class Exp3(BaseBandit):
             The history id of the action.
 
         action_recommendation : list of dictionaries
-            In each dictionary, it will contains {Action object, estimated_reward, uncertainty}
+            In each dictionary, it will contains {Action object,
+            estimated_reward, uncertainty}
         """
 
         if self.exp3_ is None:
@@ -118,16 +124,22 @@ class Exp3(BaseBandit):
 
         query_vector = np.array([score[act] for act in self.action_ids])
         action_recommendation = []
-        action_recommendation_ids = np.random.choice(self.action_ids,
-                            size=n_actions, p=query_vector, replace=False)
+        action_recommendation_ids = np.random.choice(
+            self.action_ids, size=n_actions, p=query_vector, replace=False)
 
         for action_id in action_recommendation_ids:
             action_id = int(action_id)
-            action = [action for action in self._actions if action.action_id == action_id][0]
-            action_recommendation.append({'action': action, 'estimated_reward': estimated_reward[action_id],
-                                     'uncertainty': uncertainty[action_id], 'score': score[action_id]})
+            action = [action for action in self._actions
+                      if action.action_id == action_id][0]
+            action_recommendation.append({
+                'action': action,
+                'estimated_reward': estimated_reward[action_id],
+                'uncertainty': uncertainty[action_id],
+                'score': score[action_id]
+            })
 
-        history_id = self._historystorage.add_history(context, action_recommendation, reward=None)
+        history_id = self._historystorage.add_history(
+            context, action_recommendation, reward=None)
         return history_id, action_recommendation
 
     def reward(self, history_id, rewards):
@@ -152,7 +164,8 @@ class Exp3(BaseBandit):
             for i in actions_id:
                 rhat[i] = 0.0
             rhat[action_id] = reward_tmp / query_vector[action_id]
-            w[action_id] *= np.exp(self.gamma * rhat[action_id] / len(self.action_ids))
+            w[action_id] *= \
+                np.exp(self.gamma * rhat[action_id] / len(self.action_ids))
 
         self._modelstorage.save_model({'query_vector': query_vector, 'w': w})
 

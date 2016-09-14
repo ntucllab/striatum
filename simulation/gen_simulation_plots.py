41525b380d68a5d10e8a85a3336338c41bdc13e9
yangarbiter <yangarbiter@gmail.com>
2016-09-14 13:43:03 +0800

    script to generate simulation plots for docs

---
+++ simulation/gen_simulation_plots.py
@@ -0,0 +1,27 @@
+"""This file is dedicated to to generate simulation doc page plots.
+"""
+from os.path import join
+
+import matplotlib.pyplot as plt
+
+import simulation_exp3
+import simulation_exp4p
+import simulation_linthompsamp
+import simulation_linucb
+import simulation_ucb1
+
+def main():
+    import argparse
+    parser = argparse.ArgumentParser(description='Generate features.')
+    parser.add_argument('-p', '--path', default="",
+                        help="the path of simulation result plots to save")
+    args = parser.parse_args()
+
+    simulation_exp3.simulate_bandit()
+    for sim in [simulation_exp3, simulation_exp4p, simulation_linthompsamp,
+                simulation_linucb, simulation_ucb1]:
+        plt.savefig(join(args.path, "%s" % sim.__name__))
+        plt.close()
+
+if __name__ == "__main__":
+    main()

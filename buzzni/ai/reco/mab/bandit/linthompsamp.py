14ffd2fba86f05068885b92d472f9cb44ea999d7
SeonbeomKim <jeffrey@buzzni.com>
2021-07-07 20:01:19 +0900

    tf-agents 기반으로 수정 (decay 등등), LinearBandit 상속

--- buzzni/ai/reco/mab/bandit/linthompsamp.py
+++ buzzni/ai/reco/mab/bandit/linthompsamp.py
@@ -6,18 +6,16 @@ is linear. The sampling method is used to balance the exploration and
 exploitation. Please check the reference for more details.
 """
 import logging
-import six
-from six.moves import zip
 
 import numpy as np
 
-from buzzni.ai.reco.mab.bandit.bandit import BaseBandit
+from buzzni.ai.reco.mab.bandit.bandit import LinearBandit
 from buzzni.ai.reco.mab.utils import get_random_state
 
 LOGGER = logging.getLogger(__name__)
 
 
-class LinThompSamp(BaseBandit):
+class LinThompSamp(LinearBandit):
     r"""Thompson sampling with linear payoff.
 
     Parameters
@@ -35,18 +33,14 @@ class LinThompSamp(BaseBandit):
         The class used to initiate the recommendations. If None, then use
         default Recommendation class.
 
-    delta: float, 0 < delta < 1
-        With probability 1 - delta, LinThompSamp satisfies the theoretical
-        regret bound.
+    alpha: float
+            The constant determines the width of the upper confidence bound. (follow tf-agents implement)
 
-    R: float, R >= 0
-        Assume that the residual  :math:`ri(t) - bi(t)^T \hat{\mu}`
-        is R-sub-gaussian. In this case, R^2 represents the variance for
-        residuals of the linear model :math:`bi(t)^T`.
+    gamma: float
+        forgetting factor in [0.0, 1.0]. When set to 1.0, the algorithm does not forget.
 
-    epsilon: float, 0 < epsilon < 1
-        A  parameter  used  by  the  Thompson Sampling algorithm.
-        If the total trials T is known, we can choose epsilon = 1/ln(T).
+    tikhonov_weight: float
+        tikhonov regularization term.
 
     random_state: {int, np.random.RandomState} (default: None)
         If int, np.random.RandomState will used it as seed. If None, a random
@@ -60,171 +54,46 @@ class LinThompSamp(BaseBandit):
     """
 
     def __init__(self, history_storage, model_storage, action_storage,
-                 recommendation_cls=None, context_dimension=128, delta=0.5,
-                 R=0.01, epsilon=0.5, random_state=None):
-        super(LinThompSamp, self).__init__("LinThompSamp", history_storage, model_storage,
-                                           action_storage, recommendation_cls)
+                 recommendation_cls=None, context_dimension=128,
+                 alpha=1.0, gamma=1.0, tikhonov_weight=1.0, random_state=None):
+        self.alpha = alpha
+        self.tikhonov_weight = tikhonov_weight
         self.random_state = get_random_state(random_state)
-        self.context_dimension = context_dimension
 
-        # 0 < delta < 1
-        if not isinstance(delta, float):
-            raise ValueError("delta should be float")
-        elif (delta < 0) or (delta >= 1):
-            raise ValueError("delta should be in (0, 1]")
-        else:
-            self.delta = delta
+        if gamma < 0.0 or gamma > 1.0:
+            raise ValueError('Forgetting factor `gamma` must be in [0.0, 1.0].')
 
-        # R > 0
-        if not isinstance(R, float):
-            raise ValueError("R should be float")
-        elif R <= 0:
-            raise ValueError("R should be positive")
-        else:
-            self.R = R  # pylint: disable=invalid-name
+        super(LinThompSamp, self).__init__(model_name="LinThompSamp",
+                                           history_storage=history_storage,
+                                           model_storage=model_storage,
+                                           action_storage=action_storage,
+                                           context_dimension=context_dimension,
+                                           gamma=gamma,
+                                           recommendation_cls=recommendation_cls)
 
-        # 0 < epsilon < 1
-        if not isinstance(epsilon, float):
-            raise ValueError("epsilon should be float")
-        elif (epsilon < 0) or (epsilon > 1):
-            raise ValueError("epsilon should be in (0, 1)")
-        else:
-            self.epsilon = epsilon
-
-        # model initialization
-        B = np.identity(self.context_dimension)  # pylint: disable=invalid-name
-        mu_hat = np.zeros(shape=(self.context_dimension, 1))
-        f = np.zeros(shape=(self.context_dimension, 1))
-        self._model_storage.save_model({'B': B, 'mu_hat': mu_hat, 'f': f})
-
-    def _linthompsamp_score(self, context):
-        """Thompson Sampling"""
-        action_ids = list(six.viewkeys(context))
-        context_array = np.asarray([context[action_id]
-                                    for action_id in action_ids])
-        model = self._model_storage.get_model()
-        B = model['B']  # pylint: disable=invalid-name
-        mu_hat = model['mu_hat']
-        v = self.R * np.sqrt(24 / self.epsilon
-                             * self.context_dimension
-                             * np.log(1 / self.delta))
-        mu_tilde = self.random_state.multivariate_normal(
-            mu_hat.flat, v**2 * np.linalg.inv(B))[..., np.newaxis]
-        estimated_reward_array = context_array.dot(mu_hat)
-        score_array = context_array.dot(mu_tilde)
-
-        estimated_reward_dict = {}
-        uncertainty_dict = {}
-        score_dict = {}
-        for action_id, estimated_reward, score in zip(
-                action_ids, estimated_reward_array, score_array):
-            estimated_reward_dict[action_id] = float(estimated_reward)
-            score_dict[action_id] = float(score)
-            uncertainty_dict[action_id] = float(score - estimated_reward)
-        return estimated_reward_dict, uncertainty_dict, score_dict
-
-    def get_action(self, context, n_actions=None):
-        """Return the action to perform
-
-        Parameters
-        ----------
-        context : dictionary
-            Contexts {action_id: context} of different actions.
-
-        n_actions: int (default: None)
-            Number of actions wanted to recommend users. If None, only return
-            one action. If -1, get all actions.
-
-        Returns
-        -------
-        history_id : int
-            The history id of the action.
-
-        recommendations : list of dict
-            Each dict contains
-            {Action object, estimated_reward, uncertainty}.
+    def _score(self, context, model):
+        """disjoint LINUCB algorithm.
         """
-        if self._action_storage.count() == 0:
-            return self._get_action_with_empty_action_storage(context,
-                                                              n_actions)
+        '''
+        tf-agents 구현체 따름
+        # 계산
+            - https://github.com/tensorflow/agents/blob/3719a8780d7054984ddc528dbfe99b51b9f10062/tf_agents/bandits/policies/linear_bandit_policy.py#L251
+        '''
 
-        if not isinstance(context, dict):
-            raise ValueError(
-                "LinThompSamp requires context dict for all actions!")
-        if n_actions == -1:
-            n_actions = self._action_storage.count()
+        A = model['A']  # pylint: disable=invalid-name
+        b = model['b']  # pylint: disable=invalid-name
 
-        estimated_reward, uncertainty, score = self._linthompsamp_score(context)
+        # The recommended actions should maximize the Linear UCB.
+        estimated_reward = {}
+        uncertainty = {}
+        score = {}
+        for action_id in self._action_storage.iterids():
+            action_context = np.reshape(context[action_id], (-1, 1))  # user feature: (dim, 1)
 
-        if n_actions is None:
-            recommendation_id = max(score, key=score.get)
-            recommendations = self._recommendation_cls(
-                action=self._action_storage.get(recommendation_id),
-                estimated_reward=estimated_reward[recommendation_id],
-                uncertainty=uncertainty[recommendation_id],
-                score=score[recommendation_id],
-            )
-        else:
-            recommendation_ids = sorted(score, key=score.get,
-                                        reverse=True)[:n_actions]
-            recommendations = []  # pylint: disable=redefined-variable-type
-            for action_id in recommendation_ids:
-                recommendations.append(self._recommendation_cls(
-                    action=self._action_storage.get(action_id),
-                    estimated_reward=estimated_reward[action_id],
-                    uncertainty=uncertainty[action_id],
-                    score=score[action_id],
-                ))
-
-        history_id = self._history_storage.add_history(context, recommendations)
-        return history_id, recommendations
-
-    def reward(self, history_id, rewards):
-        """Reward the previous action with reward.
-
-        Parameters
-        ----------
-        history_id : int
-            The history id of the action to reward.
-
-        rewards : dictionary
-            The dictionary {action_id, reward}, where reward is a float.
-        """
-        context = (self._history_storage
-                   .get_unrewarded_history(history_id)
-                   .context)
-
-        # Update the model
-        model = self._model_storage.get_model()
-        B = model['B']  # pylint: disable=invalid-name
-        f = model['f']
-
-        for action_id, reward in six.viewitems(rewards):
-            context_t = np.reshape(context[action_id], (-1, 1))
-            B += context_t.dot(context_t.T)  # pylint: disable=invalid-name
-            f += reward * context_t
-            mu_hat = np.linalg.inv(B).dot(f)
-        self._model_storage.save_model({'B': B, 'mu_hat': mu_hat, 'f': f})
-
-        # Update the history
-        self._history_storage.add_reward(history_id, rewards)
-
-    def add_action(self, actions):
-        """ Add new actions (if needed).
-
-        Parameters
-        ----------
-        actions : iterable
-            A list of Action oBjects for recommendation
-        """
-        self._action_storage.add(actions)
-
-    def remove_action(self, action_id):
-        """Remove action by id.
-
-        Parameters
-        ----------
-        action_id : int
-            The id of the action to remove.
-        """
-        self._action_storage.remove(action_id)
+            A_inv_x = np.linalg.inv(A[action_id] + self.tikhonov_weight * np.identity(self.context_dimension)).dot(
+                action_context)  # (dim, 1)
+            estimated_reward[action_id] = float(b[action_id].T.dot(A_inv_x))
+            uncertainty[action_id] = float(np.sqrt(action_context.T.dot(A_inv_x)))
+            score[action_id] = np.random.normal(loc=estimated_reward[action_id],
+                                                scale=self.alpha * uncertainty[action_id])
+        return estimated_reward, uncertainty, score
--- buzzni/ai/reco/mab/bandit/linucb.py
+++ buzzni/ai/reco/mab/bandit/linucb.py
@@ -7,14 +7,13 @@ of the context.
 import logging
 
 import numpy as np
-import six
 
-from buzzni.ai.reco.mab.bandit.bandit import BaseBandit
+from buzzni.ai.reco.mab.bandit.bandit import LinearBandit
 
 LOGGER = logging.getLogger(__name__)
 
 
-class LinUCB(BaseBandit):
+class LinUCB(LinearBandit):
     r"""LinUCB with Disjoint Linear Models
 
     Parameters
@@ -53,35 +52,22 @@ class LinUCB(BaseBandit):
 
     def __init__(self, history_storage, model_storage, action_storage,
                  recommendation_cls=None, context_dimension=128, alpha=1.0, gamma=1.0, tikhonov_weight=1.0):
-        super(LinUCB, self).__init__("LinUCB", history_storage, model_storage,
-                                     action_storage, recommendation_cls)
         self.alpha = alpha
-        self.gamma = gamma
         self.tikhonov_weight = tikhonov_weight
         self.context_dimension = context_dimension
 
-        if self.gamma < 0.0 or self.gamma > 1.0:
+        if gamma < 0.0 or gamma > 1.0:
             raise ValueError('Forgetting factor `gamma` must be in [0.0, 1.0].')
 
-        # Initialize LinUCB Model Parameters
-        model = {'A': {}, 'b': {}}
-        for action_id in self._action_storage.iterids():
-            self._init_action_model(model, action_id)
+        super(LinUCB, self).__init__(model_name="LinUCB",
+                                     history_storage=history_storage,
+                                     model_storage=model_storage,
+                                     action_storage=action_storage,
+                                     context_dimension=context_dimension,
+                                     gamma=gamma,
+                                     recommendation_cls=recommendation_cls)
 
-        self._model_storage.save_model(model)
-
-    def _init_action_model(self, model, action_id):
-        '''
-        tf-agents 구현체 따름
-        # variable 선언
-            - https://github.com/tensorflow/agents/blob/3719a8780d7054984ddc528dbfe99b51b9f10062/tf_agents/bandits/agents/linear_bandit_agent.py#L81
-        # 계산
-            - https://github.com/tensorflow/agents/blob/3719a8780d7054984ddc528dbfe99b51b9f10062/tf_agents/bandits/policies/linear_bandit_policy.py#L251
-        '''
-        model['A'][action_id] = np.zeros(shape=(self.context_dimension, self.context_dimension))
-        model['b'][action_id] = np.zeros((self.context_dimension, 1))
-
-    def _linucb_score(self, context):
+    def _score(self, context, model):
         """disjoint LINUCB algorithm.
         """
         '''
@@ -90,9 +76,6 @@ class LinUCB(BaseBandit):
             - https://github.com/tensorflow/agents/blob/3719a8780d7054984ddc528dbfe99b51b9f10062/tf_agents/bandits/policies/linear_bandit_policy.py#L251
         '''
 
-        # A_inv[action_id] = np.linalg.inv(A[action_id])
-        # theta[action_id] = A_inv[action_id].dot(b[action_id])
-        model = self._model_storage.get_model()
         A = model['A']  # pylint: disable=invalid-name
         b = model['b']  # pylint: disable=invalid-name
 
@@ -106,131 +89,7 @@ class LinUCB(BaseBandit):
             A_inv_x = np.linalg.inv(A[action_id] + self.tikhonov_weight * np.identity(self.context_dimension)).dot(
                 action_context)  # (dim, 1)
             estimated_reward[action_id] = float(b[action_id].T.dot(A_inv_x))
-            uncertainty[action_id] = float(self.alpha * np.sqrt(action_context.T.dot(A_inv_x)))
+            uncertainty[action_id] = float(np.sqrt(action_context.T.dot(A_inv_x)))
             score[action_id] = (estimated_reward[action_id]
-                                + uncertainty[action_id])
+                                + self.alpha * uncertainty[action_id])
         return estimated_reward, uncertainty, score
-
-    def get_action(self, context, n_actions=None):
-        """Return the action to perform
-
-        Parameters
-        ----------
-        context : dict
-            Contexts {action_id: context} of different actions.
-
-        n_actions: int (default: None)
-            Number of actions wanted to recommend users. If None, only return
-            one action. If -1, get all actions.
-
-        Returns
-        -------
-        history_id : int
-            The history id of the action.
-
-        recommendations : list of dict
-            Each dict contains
-            {Action object, estimated_reward, uncertainty}.
-        """
-        if self._action_storage.count() == 0:
-            return self._get_action_with_empty_action_storage(context,
-                                                              n_actions)
-
-        if not isinstance(context, dict):
-            raise ValueError("LinUCB requires context dict for all actions!")
-        if n_actions == -1:
-            n_actions = self._action_storage.count()
-
-        estimated_reward, uncertainty, score = self._linucb_score(context)
-
-        if n_actions is None:
-            recommendation_id = max(score, key=score.get)
-            recommendations = self._recommendation_cls(
-                action=self._action_storage.get(recommendation_id),
-                estimated_reward=estimated_reward[recommendation_id],
-                uncertainty=uncertainty[recommendation_id],
-                score=score[recommendation_id],
-            )
-        else:
-            recommendation_ids = sorted(score, key=score.get,
-                                        reverse=True)[:n_actions]
-            recommendations = []  # pylint: disable=redefined-variable-type
-            for action_id in recommendation_ids:
-                recommendations.append(self._recommendation_cls(
-                    action=self._action_storage.get(action_id),
-                    estimated_reward=estimated_reward[action_id],
-                    uncertainty=uncertainty[action_id],
-                    score=score[action_id],
-                ))
-
-        history_id = self._history_storage.add_history(context, recommendations)
-        return history_id, recommendations
-
-    def reward(self, history_id, rewards):
-        """Reward the previous action with reward.
-
-        Parameters
-        ----------
-        history_id : int
-            The history id of the action to reward.
-
-        rewards : dictionary
-            The dictionary {action_id, reward}, where reward is a float.
-        """
-        context = (self._history_storage
-                   .get_unrewarded_history(history_id)
-                   .context)
-
-        # Update the model
-        model = self._model_storage.get_model()
-        A = model['A']  # pylint: disable=invalid-name
-        b = model['b']
-
-        '''
-        tf-agents 구현체 따름
-        # gamma:
-            - https://github.com/tensorflow/agents/blob/3719a8780d7054984ddc528dbfe99b51b9f10062/tf_agents/bandits/agents/linear_bandit_agent.py#L110
-        '''
-
-        for action_id, reward in six.viewitems(rewards):
-            action_context = np.reshape(context[action_id], (-1, 1))
-            A[action_id] = self.gamma * A[action_id] + action_context.dot(action_context.T)
-            b[action_id] = self.gamma * b[action_id] + reward * action_context
-
-        self._model_storage.save_model({
-            'A': A,
-            'b': b
-        })
-
-        # Update the history
-        self._history_storage.add_reward(history_id, rewards)
-
-    def add_action(self, actions):
-        """ Add new actions (if needed).
-
-        Parameters
-        ----------
-        actions : iterable
-            A list of Action objects for recommendation
-        """
-        new_action_ids = self._action_storage.add(actions)
-        model = self._model_storage.get_model()
-
-        for action_id in new_action_ids:
-            self._init_action_model(model, action_id)
-
-        self._model_storage.save_model(model)
-
-    def remove_action(self, action_id):
-        """Remove action by id.
-
-        Parameters
-        ----------
-        action_id : int
-            The id of the action to remove.
-        """
-        model = self._model_storage.get_model()
-        del model['A'][action_id]
-        del model['b'][action_id]
-        self._model_storage.save_model(model)
-        self._action_storage.remove(action_id)
